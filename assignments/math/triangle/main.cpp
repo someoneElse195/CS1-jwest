068908c12e9ded6ba2f6f6248fe17e38be4f0656
SomeoneElse195 <amwest195@gmail.com>
2023-08-21 12:45:51 -0500

    HW: 2 desc and start

---
+++ assignments/math/triangle/main.cpp
@@ -0,0 +1,11 @@
+/*
+TODO: Desc of what code does.
+*/
+int Main() {
+    /*
+    TODO: Take input for 3 sides of a triangle, find area, perimeter then print all values with
+        proper descriptions for all.
+
+    TODO: Not required, do it anyway: Test if those 3 sides can actually form a triangle or not. Print results.
+    */
+}
