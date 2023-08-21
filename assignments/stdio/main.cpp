c4d1b9ebc145b217b5609bbacb77b131a0ca1c26
SomeoneElse195 <amwest195@gmail.com>
2023-08-21 12:34:07 -0500

    Planning of HW:1

---
+++ assignments/stdio/main.cpp
@@ -0,0 +1,10 @@
+/*
+TODO: DESC OF PROGRAM
+*/
+
+int Main() {
+    /*
+    TODO: Take input for name, print name out greeting user.
+    TODO: Print out all 7(?) stages of hangman, use variables to store each level.
+    */
+}
