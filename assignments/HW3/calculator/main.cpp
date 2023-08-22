770cf190ee92eef6661e3b2be49ea0bf4737c5ac
someoneElse195 <amwest195@gmail.com>
2023-08-22 15:57:48 -0600

    Sum part of HW3 done

--- assignments/HW3/calculator/main.cpp
+++ assignments/HW3/calculator/main.cpp
@@ -4,18 +4,26 @@ TODO: Write desc of program
 #include <iostream>
 #include <string>
 #include <stdlib.h>
+#include <iomanip>
 
 using namespace std;
 
 float addition(float one, float two);
 
 int main() {
-    float sum = addition(1,5);
-    cout << sum << endl;
-    /*
-    TODO: 1 Write func. that takes 2 numbers, adds them, and returns the sum. 
-        Function must accept floats & integers.
+    float numA;
+    float numB;
 
+    cout << "Please input 2 numbers" << endl;
+    cin >> numA >> numB;
+    
+    float sum = addition(numA, numB);
+    cout << fixed << showpoint;
+    cout << setprecision(2);
+    cout << "The sum of your numbers is ";
+    cout << sum << endl;
+    
+    /*
     TODO: 2 Write func. that takes 2 numbers, multiplies them, and returns the result.
         Function must accept floats & integers.
 
