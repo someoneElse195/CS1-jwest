216bcc95d761d5f5ecd09d7b48bc595fbd1e1575
SomeoneElse195 <amwest195@gmail.com>
2023-08-22 12:05:26 -0500

    Finished intial HW3 desc

---
+++ assignments/HW3/calculator/main.cpp
@@ -0,0 +1,36 @@
+/*
+TODO: Write desc of program
+*/
+
+int Main() {
+    /*
+    TODO: 1 Write func. that takes 2 numbers, adds them, and returns the sum. 
+        Function must accept floats & integers.
+
+    TODO: 2 Write func. that takes 2 numbers, multiplies them, and returns the result.
+        Function must accept floats & integers.
+
+    TODO: 3 Write func. that takes 2 numbers, divides first by second, returns result.
+        Function must accept floats & integers.
+
+    TODO: 4 Write func. that takes 2 numbers, subtracts second from first.
+        Function must accept floats & integers.
+
+    TODO: 5 Same as 3 however returns remainder.
+
+    TODO: 6 Write func. that finds the first to the power of the second and returns result.
+        Function must accept floats & integers.
+    
+    TODO: 7 Write func. that takes num and returns square root.
+        Function must accept floats & integers.
+
+    TODO: 8 Prompt user to provide 2 numbers. Call all previous functions, and print out results of each 
+        with proper descriptions.
+    
+    TODO: 9 Write func. to test each previous function with 2 different test cases. 
+        Include sc of test results.
+
+    TODO: 10 opt. Write func. to find the larger of two numbers. Use the same numbers as inputted previously.
+    */
+
+}
