3892f3190444463f3c7f52b55459408ba7d597c1
June <amwest195@gmail.com>
2023-08-23 22:34:02 -0600

    Completion of HW2

--- assignments/math/triangle/main.cpp
+++ assignments/math/triangle/main.cpp
@@ -1,11 +1,66 @@
-/*
-TODO: Desc of what code does.
-*/
-int Main() {
-    /*
-    TODO: Take input for 3 sides of a triangle, find area, perimeter then print all values with
-        proper descriptions for all.
+/*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\
+| Program to compute area and perimeter of a triangle.            |
+| The program will also output whether the sides form a triangle. |
+| Written by amwest                                               |
+\-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-*/
 
-    TODO: Not required, do it anyway: Test if those 3 sides can actually form a triangle or not. Print results.
-    */
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+float area(float a, float b, float c);
+float peri(float a, float b, float c);
+bool isTri(float a, float b, float c);
+
+int main() {
+
+    float sideA;
+    float sideB;
+    float sideC;
+
+    /* While statement to loop back around if sides are determined to not be a triangle.           \
+    \  If sides form a triangle it will output data to console and break loop to terminate program. */
+    
+    while(true) {
+
+        cout << "Please input 3 sides for a triangle: ";
+        cin >> sideA >> sideB >> sideC;
+        cout << endl;
+        if(isTri(sideA, sideB, sideC)) {
+            float triArea = area(sideA, sideB, sideC);
+
+            cout << "The area is: " << triArea << endl;
+            break;
+        } else {
+            cout << "This is not a triangle. ";
+        }
+
+    }
+
+
+}
+
+//Computes area of triangle.
+
+float area(float a, float b, float c) {
+    float area =(.25 * sqrt((a + b + c) * (b - a + c) * (a - b + c) * (a + b - c)));
+    return area;
+}
+
+//Computes perimeter of triangle.
+
+float peri(float a, float b, float c) {
+    float peri = a + b + c;
+    return peri;
+}
+
+//Determines whether inputted numbers form a triangle.
+
+bool isTri(float a, float b, float c) {
+    if(a < (b+c) && b < (a+c) && c < (a+b)) {
+        return true;
+    } else {
+        return false;
+    }
 }
