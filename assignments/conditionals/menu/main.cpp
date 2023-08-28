1eab93e69f130e3988c0029a3b1b4fbe9c0d9ba3
someoneElse195 <amwest195@gmail.com>
2023-08-28 16:52:04 -0600

    comput done. work on IO menu

--- assignments/conditionals/menu/main.cpp
+++ assignments/conditionals/menu/main.cpp
@@ -5,20 +5,67 @@
 \*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int max(int one);
+void max(float nums[5]);
+void min(float nums[5]);
+void average(float nums[5]);
+void oddity(float nums[5]);
+
 
 int main() {
+    string name;
+    float numbers [5];
+
+    cout << "Hello, please enter your name: " << endl; 
+    cin >> name;
+    cout << endl << "Welcome, " << name << "." << endl << "Please enter 5 numbers: ";
+    cin >> numbers[0] >> numbers[1] >> numbers[2] >> numbers[3] >> numbers[4];
+    cout << endl;
+}
+
+void max(float nums[5]) {
+    float max = nums[0];
+    for(int i = 1; i < 5; i++) {
+        if(nums[i] > max) {
+            max = nums[i];
+        } else {
+            continue;
+        }
+    }
+    cout << max << endl;
 
 }
 
-int max(int[] nums[]) {
-    int max = nums[0];
-    for(int i = 0; i < 5; i++) {
-        
+void min(float nums[5]) {
+    float min = nums[0];
+    for(int i = 1; i < 5; i++) {
+        if(nums[i] < min) {
+            min = nums[i];
+        } else {
+            continue;
+        }
     }
+    cout << min << endl;
 
-    return max;
+}
+
+void average(float nums[5]) {
+    float avg = (nums[0] + nums[1] + nums[2] + nums[3] + nums[4])/5.00;
+
+    cout << avg << endl;
+}
+
+void oddity(float nums[5]) {
+    int num = (nums[0] + nums[1] + nums[2] + nums[3] + nums[4]);
+    if(num == 0) {
+        cout << "The floor of the sum of your numbers is 0." << endl;
+    } else if(num%2 == 1) {
+        cout << "The floor of the sum of your numbers is odd." << endl;
+    } else if (num%2 == 0){
+        cout << "The floor of the sum of your numbers is even." << endl;
+    }
+    
 }
