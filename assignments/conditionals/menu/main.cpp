2f86bc170188a47ba13071ce65eb217b2da33849
someoneElse195 <amwest195@gmail.com>
2023-08-28 10:54:50 -0600

    Literally dont remember what i was doing

---
+++ assignments/conditionals/menu/main.cpp
@@ -0,0 +1,24 @@
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*\
+| CLI that computes sum, product, max, min,   |
+| average, & odditty of any 5 given numbers.  |
+| Written by amwest.                          |
+\*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+#include <iostream>
+
+using namespace std;
+
+int max(int one);
+
+int main() {
+
+}
+
+int max(int[] nums[]) {
+    int max = nums[0];
+    for(int i = 0; i < 5; i++) {
+        
+    }
+
+    return max;
+}
