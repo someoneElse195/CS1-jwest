20b86b34dcaf08dd064bea67155175b11bdd7724
June <amwest195@gmail.com>
2023-09-06 11:17:50 -0600

    fixing ascii lab due to change in instructions or maybe i missed something

--- labs/ascii/main.cpp
+++ labs/ascii/main.cpp
@@ -10,10 +10,21 @@ void printASCII();
 void extraASCII();
 
 int main() {
-    
+    string name;
+    cout << "Hello, please enter your name: ";
+    cin >> name;
+    cout << "\nNice meeting you, " << name;
+    cout << "\nHope you like my Tom & Jerry ASCII art...\n\n";
     printASCII();
     extraASCII();
 
+    
+    cout << "\nGood bye... hit enter to exit the program: " << '\n';
+    cin.ignore(100, '\n');
+    cin.get();
+    cout << "See ya\n\n";
+
+    return 0;
 }
 
 
@@ -29,19 +40,19 @@ void printASCII() {
 }
 
 void extraASCII() {
-    string L1 = "                             ******       ******";
-    string L2 = "                           **********   **********";
-    string L3 = "                         ************* *************";
-    string L4 = "                        *****************************";
-    string L5 = "                        *****************************";
-    string L6 = "                        *****************************";
-    string L7 = "                         ***************************";
-    string L8 = "                           ***********************";
-    string L9 = "                             *******************";
-    string L10= "                               ***************";
-    string L11= "                                 ***********";
-    string L12= "                                   *******";
-    string L13= "                                     ***";
+    string L1 = "                             _______      _______";
+    string L2 = "                          /*********\\   /*********\\";
+    string L3 = "                         /***********\\ /***********\\";
+    string L4 = "                        /***************************\\";
+    string L5 = "                        |***************************|";
+    string L6 = "                        |***************************|";
+    string L7 = "                         \\*************************/";
+    string L8 = "                           \\*********************/";
+    string L9 = "                             \\*****************/";
+    string L10= "                               \\*************/";
+    string L11= "                                 \\*********/";
+    string L12= "                                   \\*****/";
+    string L13= "                                     \\*/";
     cout << L1 << endl << L2 << endl << L3 << endl << L4 << endl << L5 << endl << L6 << endl << L7 << endl << L8 << endl << L9 << endl << L10 << endl << L11 << endl << L12 << endl << L13 << endl;
 }
 
