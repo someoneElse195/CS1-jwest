ccab7c71f3c1a8673d69209306630e5db265c21d
June <amwest195@gmail.com>
2023-11-30 09:13:52 -0700

    Cells all working now to figure out buttons and checking for clicks

--- FINAL/main.cpp
+++ FINAL/main.cpp
@@ -1,6 +1,9 @@
 #include <wx/wx.h>
 #include <wx/sizer.h>
 
+
+
+
 class BasicDrawPane : public wxPanel
 {
     
@@ -9,18 +12,36 @@ public:
     
     void paintEvent(wxPaintEvent & evt);
     void paintNow();
-    
+
+
     void render(wxDC& dc);
+
+    int Cells[3][3] = {
+        {-1,-1,-1},
+        {-1,-1,-1},
+        {-1,-1,-1}
+    };
     
+    wxButton spaceButton00(wxWindow parent,
+    wxWindowID wxID_HELP, 
+    wxString label, 
+    wxPoint pos, 
+    wxSize size, 
+    long style, 
+    wxValidator validator, 
+    wxString name);
+
+
     DECLARE_EVENT_TABLE()
 };
-
 
 class MyApp: public wxApp
 {
     bool OnInit();
-    
-    BasicDrawPane * drawPane;
+
+
+
+    BasicDrawPane *drawPane;
 public:
         
 };
@@ -33,6 +54,7 @@ public:
     MyFrame();
  
 private:
+
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
 };
@@ -45,6 +67,9 @@ bool MyApp::OnInit()
     MyFrame *frame = new MyFrame();
     drawPane = new BasicDrawPane( (wxFrame*) frame );
     frame->Show(true);   
+
+
+
     return true;
     
 } 
@@ -79,7 +104,7 @@ MyFrame::MyFrame()
     wxMenuBar *menuBar = new wxMenuBar;
     menuBar->Append(menuFile, "&File");
     menuBar->Append(menuHelp, "&Help");
- 
+
     SetMenuBar( menuBar );
  
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
@@ -93,6 +118,7 @@ BEGIN_EVENT_TABLE(BasicDrawPane, wxPanel)
 
 // catch paint events
 EVT_PAINT(BasicDrawPane::paintEvent)
+
 
 END_EVENT_TABLE()
 
@@ -117,9 +143,31 @@ void BasicDrawPane::paintNow()
 void BasicDrawPane::render(wxDC&  dc)
 {
     // draw a line
-    dc.SetPen( wxPen( wxColor(255,0,0), 10 ) ); 
+    dc.SetPen( wxPen( wxColor(0,0,0), 10 ) ); 
     dc.DrawLine( 50, 150, 350, 150 ); 
     dc.DrawLine(150, 50, 150, 350);
     dc.DrawLine(250, 50, 250, 350);
     dc.DrawLine(50, 250, 350, 250);
+
+    // dc.DrawRectangle(75,75,50,50);
+    // dc.DrawRectangle(175,75, 50,50);
+    // dc.DrawLine(75, 75, 125, 125);
+    // dc.DrawLine(125, 75, 75, 125);
+
+    dc.SetPen( wxPen(wxColor(65,65,65), 3));
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if (Cells[i][j] == -1) {
+                dc.DrawRectangle(75+(j*100),75+(i*100),50,50);
+            } else if (Cells[i][j] == 0) {
+                dc.DrawLine(75+(j*100), 75+(i*100), 125+(j*100), 125+(i*100));
+                dc.DrawLine(125+(j*100), 75+(i*100), 75+(j*100), 125+(i*100));
+            } else if(Cells[i][j] == 1) {
+                dc.DrawCircle(100+(j*100), 100+(i*100), 30);
+            }
+        }
+    }
+    
+    
+
 }
